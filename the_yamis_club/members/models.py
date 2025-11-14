54e5a98dbcd62269541cab29765799eb67944020
Yamisipq <rosasyamith@gmail.com>
2025-11-14 10:29:41 -0500

    Tercer skibidi

--- the_yamis_club/members/models.py
+++ the_yamis_club/members/models.py
@@ -5,3 +5,6 @@ class Member(models.Model):
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null = True)
   joined_date = models.DateField(null = True)
+  pokemon_fav = models.CharField(max_length=25, null = True)
+  gender = models.CharField(max_length=10, null = True)
+  doc = models.CharField(max_length=15, null = True)
