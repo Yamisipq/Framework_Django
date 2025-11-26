1c424d056342ada8440eeeed9939aa12acc13aeb
Yamith Rosas <rosasyamith@gmail.com>
2025-11-26 07:36:56 -0500

    mi gente no se cuantos commits voy pipipipi

--- the_yamis_club/members/views.py
+++ the_yamis_club/members/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.db.models import Q
 
 def members(request):
   mymembers = Member.objects.all().values()
@@ -21,3 +22,33 @@ def details(request, id):
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
+
+def testing(request):
+  template = loader.get_template('template.html')
+  miembro = Member.objects.all().values()
+  columna_firstname = Member.objects.values_list('firstname')
+  espe = Member.objects.filter(firstname='Yamith', lastname='Rosas').values()
+  mydata = Member.objects.filter(Q(firstname='Elyan') | Q(firstname='Davidson')).values()
+  ski=Member.objects.filter(firstname__startswith='L').values()
+  tilin = Member.objects.filter(lastname__icontains='ez').values()
+  xd = Member.objects.filter(firstname__endswith='s').values()
+  nose = Member.objects.filter(id__range=(2, 4)).values()
+  alo = Member.objects.filter(id__lte=3).values()
+  order_by = Member.objects.all().order_by('firstname').values()
+  order_by_desc = Member.objects.all().order_by('-firstname').values()
+  
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'], 
+    'miembros' : miembro,
+    'columna_firstname': columna_firstname,
+    'espe': espe,
+    'mydata': mydata,
+    'tilin': tilin,
+    'ski': ski,
+    'xd': xd,
+    'nose': nose,
+    'alo': alo,
+    'order_by': order_by,
+    'order_by_desc': order_by_desc,
+  }
+  return HttpResponse(template.render(context, request))
